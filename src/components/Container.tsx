b93949a13e4d6d913cf2d5344fd1b1fbd76e412e
Sebastian Krawiec <sebastian.arielk@gmail.com>
2024-09-03 20:51:13 -0300

    props rework

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import { classicNameResolver } from 'typescript';
 
-interface Container {
+
+interface ContainerProps {
 width:string,
 height:string,
 flex:string,
@@ -12,20 +12,22 @@ align: string,
 padding: string,
 margin: string,
 extraStyles: string,
+self:string,
 children: React.ReactElement | JSX.Element
 class:"string"
 }
 
-export const Container = styled.div<Container>`
+export const Container = styled.div<ContainerProps>`
 
 width: ${(p) => p.width && p.width};
 height: ${(p) => p.height && p.height};
 display: ${(p) => p.flex && "flex"};
-flex-direction: ${(p) => p.column ? "column" : "row"};
-justify-content: ${(p) => p.justify && p.justify};
-align-items: ${(p) => p.align && p.align};
+flex-direction: ${(p) => p.flex && p.column ? "column" : "row"};
+justify-content: ${(p) => p.flex && p.justify && p.justify};
+align-items: ${(p) => p.flex && p.align && p.align};
 padding: ${(p) => p.padding && p.padding};
 margin: ${(p) => p.margin && p.margin};
+align-self: ${(p) => p.flex && p.self };
 ${(p) => p.extraStyles && p.extraStyles};
 
 `;

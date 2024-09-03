0ad5df252e01b84e116c27bc0b278362e2e50d6b
Sebastian Krawiec <sebastian.arielk@gmail.com>
2024-09-03 20:51:53 -0300

    theme consume

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
 import { Container } from "./components";
 import backgroundPattern from "./assets/backgroundPattern.png"
 
 
 
-const fatherExtraStyles = {
-
-}
-
-console.log(backgroundPattern)
-
 function App() {
+
+  const theme = useTheme();
+
   return (
-    <>
-    
+
     <AppContainer
     flex
     column
     height={"fit-content"}
-    extraStyles={fatherExtraStyles}
     align={"center"}
     padding={"0px 24px"}
     >
@@ -28,10 +23,42 @@ function App() {
       width={"100%"}
       height={"100vh"}
       justify={"space-between"}
-      style={{gap:"37px"}}
+      style={{gap:theme.gaps.cardStreets}}
       >
-        <Container style={{backgroundColor:"blue", marginTop:"24px", height:"100%"}}>first boy</Container>
-        <Container style={{backgroundColor:"orange", marginBottom:"24px", height:"30%"}}>second body</Container>
+        <UpRow
+        flex
+        justify={"space-between"}
+        >
+          
+          <MainCard
+          flex 
+          column
+          justify={"space-between"}
+          >
+              right
+          </MainCard>
+          <RightWrapper 
+          flex
+          column
+          justify={"space-between"}
+          align={"center"}
+          >
+              <Container flex width="20rem" height="3rem" self="flex-end" align="center" style={{backgroundColor:"lightcoral"}}>
+                nav
+              </Container>
+              <Container>
+              graphic
+              </Container>
+              <ExtraData flex width="100%" align="center" height="10rem">
+                extra data
+              </ExtraData>
+          </RightWrapper>
+
+        </UpRow>
+        
+        <BottomRow flex justify={"space-between"} align={"center"}>
+          second body
+        </BottomRow>
 
       </Container>
       <Container
@@ -39,15 +66,18 @@ function App() {
       width={"100%"}
       height={"100vh"}
       style={{backgroundColor:"orange"}}
-      >explination</Container>
+      >explination
+      </Container>
+      
       <Container
       
       width={"100%"}
       height={"100vh"}
       style={{backgroundColor:"gray"}}
-      >about this</Container>
+      >about this
+      </Container>
     </AppContainer>
-    </>
+  
   );
 }
 
@@ -60,4 +90,38 @@ background-size: 6%;
   &:-webkit-scrollbar {
   display: none;
 }
+`;
+
+const UpRow = styled(Container)`
+margin-top:${(p) => p.theme.margins.large}; 
+height:100%;
+gap:${(p) => p.theme.gaps.cardStreets};
+`;
+
+const BottomRow = styled(Container)`
+background-color:${(p) => p.theme.colors.orange500};
+ margin-bottom:${(p) => p.theme.margins.large}; 
+ height:40%;
+ box-shadow: ${(p) => (p.theme.shadows.black,p.theme.shadows.colorized(p.theme.colors.orange500))};
+`;
+
+
+const MainCard = styled(Container)`
+
+background-color:${(p) => p.theme.colors.orange300};
+ width:35%;
+ border-radius: ${(p) => p.theme.borderRadius.common};
+ padding: 24px 25px 24px 40px;
+ box-shadow: ${(p) => (p.theme.shadows.black,p.theme.shadows.colorized(p.theme.colors.orange300))};
+`;
+
+const RightWrapper = styled(Container)`
+width:100%;
+gap: ${(p) => p.theme.gaps.cardStreets};
+`;
+
+const ExtraData = styled(Container)`
+background-color:${(p) => p.theme.colors.orange200};
+border-radius: ${(p) => p.theme.borderRadius.common};
+box-shadow: ${(p) => (p.theme.shadows.black,p.theme.shadows.colorized(p.theme.colors.orange200))};
 `;

0cca5ebf5621400294ca727bc65d8246a1b512f8
Sebastian Krawiec <sebastian.arielk@gmail.com>
2024-09-07 20:30:36 -0300

    icons added

---
+++ src/assets/icons/index.js
@@ -0,0 +1,9 @@
+import { ReactComponent as ActualLocation } from "./ActualLocation.svg";
+import { ReactComponent as Search }  from "./Search.svg";
+import { ReactComponent as Location }  from "./Location.svg";
+
+export const icons = {
+    ActualLocation,
+    Search,
+    Location
+}

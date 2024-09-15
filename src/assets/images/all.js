00a6520f1b67cc13398e2ed12b4a49faaae4736b
Sebastian Krawiec <sebastian.arielk@gmail.com>
2024-09-15 03:26:05 -0300

    new images added

--- src/assets/images/all.js
+++ src/assets/images/all.js
@@ -1,25 +1,29 @@
 import cloudy from "./cloudy.png";
-import day_cloudy from "./day_cloudy.png"
-import day_rainy from "./day_rainy.png"
-import moon from "./moon.png"
-import night_cloudy from "./night_cloudy.png"
-import night_rainy from "./night_rainy.png"
-import rainy_storm from "./rainy_storm.png"
-import rainy from "./rainy.png"
-import storm from "./storm.png"
-import sunny from "./sunny.png"
-
+import day_cloudy from "./day_cloudy.png";
+import day_rainy from "./day_rainy.png";
+import moon from "./moon.png";
+import night_cloudy from "./night_cloudy.png";
+import night_rainy from "./night_rainy.png";
+import rainy_storm from "./rainy_storm.png";
+import rainy from "./rainy.png";
+import storm from "./storm.png";
+import sunny from "./sunny.png";
+import cloud from "./cloud.png";
+import mist from "./mist.png";
+import snowy from "./snowy.png";
 
 export {
-    cloudy,
-    day_cloudy,
-    day_rainy,
-    moon,
-    night_cloudy,
-    night_rainy,
-    rainy_storm,
-    rainy,
-    storm,
-    sunny
-
+  cloudy,
+  day_cloudy,
+  day_rainy,
+  moon,
+  night_cloudy,
+  night_rainy,
+  rainy_storm,
+  rainy,
+  storm,
+  sunny,
+  cloud,
+  snowy,
+  mist,
 };

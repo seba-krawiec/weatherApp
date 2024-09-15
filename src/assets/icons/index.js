5624693f28972cce49d46a57a50260f51621bc23
Sebastian Krawiec <sebastian.arielk@gmail.com>
2024-09-15 16:39:30 -0300

    added extra data icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,9 +1,21 @@
 import { ReactComponent as ActualLocation } from "./ActualLocation.svg";
-import { ReactComponent as Search }  from "./Search.svg";
-import { ReactComponent as Location }  from "./Location.svg";
+import { ReactComponent as Search } from "./Search.svg";
+import { ReactComponent as Location } from "./Location.svg";
+import { ReactComponent as Humidity } from "./Humidity.svg";
+import { ReactComponent as FeelsLike } from "./FeelsLike.svg";
+import { ReactComponent as Precipitations } from "./Precipitations.svg";
+import { ReactComponent as Sunrise } from "./Sunrise.svg";
+import { ReactComponent as Sunset } from "./Sunset.svg";
+import { ReactComponent as Wind } from "./Wind.svg";
 
 export const icons = {
-    ActualLocation,
-    Search,
-    Location
-}
+  ActualLocation,
+  Search,
+  Location,
+  Humidity,
+  FeelsLike,
+  Precipitations,
+  Sunrise,
+  Sunset,
+  Wind,
+};

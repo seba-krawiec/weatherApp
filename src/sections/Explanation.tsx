f2c436c338dfa996b281b21e4c3b4fd7b9361a20
Sebastian Krawiec <sebastian.arielk@gmail.com>
2024-10-24 18:08:47 -0300

    wip content

--- src/sections/Explanation.tsx
+++ src/sections/Explanation.tsx
@@ -1,21 +1,43 @@
-import React from 'react'
-import styled, { useTheme } from 'styled-components'
-import { Container } from '../components'
-
+import React from "react";
+import styled, { useTheme } from "styled-components";
+import { Container } from "../components";
 
 export const Explanation = () => {
-
   const theme = useTheme();
 
   return (
     <Container
       flex
-    width={"100%"}
-    height={"100vh"}
+      column
+      align="center"
+      width={"100%"}
+      height={"100vh"}
+      gap="3rem"
+      style={{ scrollSnapAlign: "start" }}
+      padding="4rem 0"
     >
-      <div style={{backgroundColor:"orange",height:"100%", width:"100%",  scrollSnapAlign: "start"}}>
-        explanation
-      </div>
+      <Container
+        flex
+        width="100%"
+        height="30%"
+        style={{ backgroundColor: "orange" }}
+      >
+        <p>explicacion del proyecto</p>
+        <Container width="20rem">
+          <img src="" alt="foto explicacion 1" />
+        </Container>
+      </Container>
+      <Container
+        flex
+        width="100%"
+        height="30%"
+        style={{ flexDirection: "row-reverse", backgroundColor: "orangered" }}
+      >
+        <p>explicacion del proyecto 2</p>
+        <Container width="20rem">
+          <img src="" alt="foto explicacion 1" />
+        </Container>
+      </Container>
     </Container>
-  )
-}
+  );
+};

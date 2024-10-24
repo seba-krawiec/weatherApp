adc0eca3cb9769aa547ab0fa6afaee0cf34102bf
Sebastian Krawiec <sebastian.arielk@gmail.com>
2024-10-24 18:08:39 -0300

    clean input on blur

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,73 +1,66 @@
-import React from 'react'
-import { Container } from './Container.tsx'
-import styled from 'styled-components';
-import {icons} from "../assets/icons";
+import React from "react";
+import { Container } from "./Container.tsx";
+import styled from "styled-components";
+import { icons } from "../assets/icons";
 
 export const Navbar = () => {
-
-    const {Location, Search} = icons;
+  const { Location, Search } = icons;
 
   return (
-    <NavBar 
-    flex
-    self="flex-end" 
-    align="center"
-    justify="space-between"
-    >
-        <Wrapper flex justify='space-between' align='center'>
-
-                <span>Buenos Aires</span>
-                <Location/>
-                <Separator/>
-        <SearchContainer flex align='center'>
-                <button type="submit">
-                <Search/>
-                </button>
-                <input placeholder='search location' type="text" />
+    <NavBar flex self="flex-end" align="center" justify="space-between">
+      <Wrapper flex justify="space-between" align="center">
+        <span>Buenos Aires</span>
+        <Location />
+        <Separator />
+        <SearchContainer flex align="center">
+          <button type="submit">
+            <Search />
+          </button>
+          <input
+            placeholder="search location"
+            type="text"
+            onBlur={(e) => (e.target.value = "")}
+          />
         </SearchContainer>
-        </Wrapper>
-        <UnitContainer flex>
-            <h2>C</h2>
-            <h2>F</h2>
-
-        </UnitContainer>
-
-
+      </Wrapper>
+      <UnitContainer flex>
+        <h2>C</h2>
+        <h2>F</h2>
+      </UnitContainer>
     </NavBar>
-  )
-}
-
+  );
+};
 
 const NavBar = styled(Container)`
-    gap: 1.5rem;
-
+  gap: 1.5rem;
 `;
 
 const Wrapper = styled(Container)`
-    gap: 1rem;
-    box-shadow: ${(p) => (p.theme.shadows.black, p.theme.shadows.colorized(p.theme.colors.orange300))};
-    background-color:${p => p.theme.colors.orange300};
-    border-radius: 1.5rem;
-    padding: ${p => `${p.theme.paddings.medium}`};
-    color: white;
-    font-size: 2rem;
-
+  gap: 1rem;
+  box-shadow: ${(p) => (
+    p.theme.shadows.black, p.theme.shadows.colorized(p.theme.colors.orange300)
+  )};
+  background-color: ${(p) => p.theme.colors.orange300};
+  border-radius: 1.5rem;
+  padding: ${(p) => `${p.theme.paddings.medium}`};
+  color: white;
+  font-size: 2rem;
 `;
 
 const Separator = styled.div`
-    background-color: white;
-    width: .4rem;
-    height: 2.5rem;
-    border-radius:1rem;
+  background-color: white;
+  width: 0.4rem;
+  height: 2.5rem;
+  border-radius: 1rem;
 `;
 
 const SearchContainer = styled(Container)`
-position: relative;
-overflow-x: hidden;
-transition: width ease-in-out 300ms;
-width: 2.5rem;
+  position: relative;
+  overflow-x: hidden;
+  transition: width ease-in-out 300ms;
+  width: 2.5rem;
 
-button{
+  button {
     background-color: transparent;
     border: none;
     outline: none;
@@ -75,21 +68,20 @@ button{
     width: 2.5rem;
     margin-left: auto;
 
-    svg{
-        transform: scale(1.1);
-        cursor: pointer;
+    svg {
+      transform: scale(1.1);
+      cursor: pointer;
     }
-}
+  }
 
-
-input{
+  input {
     position: absolute;
     top: 0;
     left: 0;
     bottom: 0;
     display: flex;
     flex-grow: 1;
-    padding: 0 .8rem .5rem 0;
+    padding: 0 0.8rem 0.5rem 0;
     background-color: transparent;
     border: none;
     opacity: 0;
@@ -97,48 +89,45 @@ input{
     font-size: 2rem;
     cursor: pointer;
 
-    &:focus{
-        outline: 0;
+    &:focus {
+      outline: 0;
     }
-}
+  }
 
-&:focus-within{
+  &:focus-within {
     width: 20rem;
-    & input{
-        opacity: 1;
-        border-bottom: solid white .2rem;
-        cursor: initial;
-        width: calc(100% - 2.5rem);
+    & input {
+      opacity: 1;
+      border-bottom: solid white 0.2rem;
+      cursor: initial;
+      width: calc(100% - 2.5rem);
     }
-}
-
+  }
 `;
 
 const UnitContainer = styled(Container)`
+  gap: 1rem;
 
-gap: 1rem;
-
-   h2{
+  h2 {
     display: inline-block;
-    background-color:${p => p.theme.colors.orange300};
-    padding: ${p => `${p.theme.paddings.medium}`};
+    background-color: ${(p) => p.theme.colors.orange300};
+    padding: ${(p) => `${p.theme.paddings.medium}`};
     border-radius: 1rem;
-    box-shadow: ${(p) => (p.theme.shadows.black, p.theme.shadows.colorized(p.theme.colors.orange300))};
+    box-shadow: ${(p) => (
+      p.theme.shadows.black, p.theme.shadows.colorized(p.theme.colors.orange300)
+    )};
     height: 5rem;
     font-size: 2rem;
     color: whitesmoke;
     cursor: pointer;
-    transition: all .2s;
+    transition: all 0.2s;
 
-    &:hover{
-        transform: scale(1.1) translateY(-.5rem);
+    &:hover {
+      transform: scale(1.1) translateY(-0.5rem);
     }
 
-    &:active{
-        transform: scale(1) translateY(-.1rem);
+    &:active {
+      transform: scale(1) translateY(-0.1rem);
     }
-        }
-
-
-
+  }
 `;

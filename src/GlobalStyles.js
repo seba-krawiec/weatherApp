27bcf35d70904ea91df99f9f5c438cb85e43f2ee
Sebastian Krawiec <sebastian.arielk@gmail.com>
2024-10-24 18:08:02 -0300

    removed variables

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -20,23 +20,4 @@ export const GlobalStyles = createGlobalStyle`
     box-sizing: inherit;
     font-family: sans-serif;
   }
-
-
-:root {
-    
-        --color-orange500: "#D97A26",
-        --color-orange400: "#B98B3F",
-        --color-orange300: "#B9643F",
-        --color-orange200: "#E8C48A",
-       --color-blue500: "#111DFE",
-        --color-blue400: "#264AD9",
-        --color-blue300: "#3F4FB9",
-        --color-blue200: "#9AA5C0",
-
-        --shadow-black:
-        -webkit-box-shadow: 0px 2px 0px 0px rgba(0,0,0,0.1),
-        -moz-box-shadow: 0px 2px 0px 0px rgba(0,0,0,0.1),
-        box-shadow: 0px 2px 0px 0px rgba(0,0,0,0.1);
-    
-}
 `;

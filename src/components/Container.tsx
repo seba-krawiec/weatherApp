2e696e2c21cd21ac39943eacb3ca5699aafe6922
Sebastian Krawiec <sebastian.arielk@gmail.com>
2024-10-24 18:08:20 -0300

    added gap input

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,32 +1,31 @@
-import React from 'react'
-import styled from 'styled-components'
-
+import React from "react";
+import styled from "styled-components";
 
 interface ContainerProps {
-width?:string,
-height?:string,
-flex?:boolean,
-column?: boolean,
-justify?:string, 
-align?: string,
-padding?: string,
-margin?: string,
-extraStyles?: string,
-self?:string,
-children?:  React.ReactNode ,
+  width?: string;
+  height?: string;
+  flex?: boolean;
+  column?: boolean;
+  justify?: string;
+  align?: string;
+  padding?: string;
+  margin?: string;
+  extraStyles?: string;
+  self?: string;
+  children?: React.ReactNode;
+  gap?: string;
 }
 
 export const Container = styled.div<ContainerProps>`
-
-width: ${(p) => p.width && p.width};
-height: ${(p) => p.height && p.height};
-display: ${(p) => p.flex && "flex"};
-flex-direction: ${(p) => p.flex && p.column ? "column" : "row"};
-justify-content: ${(p) => p.flex && p.justify && p.justify};
-align-items: ${(p) => p.flex && p.align && p.align};
-padding: ${(p) => p.padding && p.padding};
-margin: ${(p) => p.margin && p.margin};
-align-self: ${(p) => p.flex && p.self };
-${(p) => p.extraStyles && p.extraStyles};
-
+  width: ${(p) => p.width && p.width};
+  height: ${(p) => p.height && p.height};
+  display: ${(p) => p.flex && "flex"};
+  flex-direction: ${(p) => (p.flex && p.column ? "column" : "row")};
+  justify-content: ${(p) => p.flex && p.justify && p.justify};
+  align-items: ${(p) => p.flex && p.align && p.align};
+  padding: ${(p) => p.padding && p.padding};
+  margin: ${(p) => p.margin && p.margin};
+  align-self: ${(p) => p.flex && p.self};
+  gap: ${(p) => p.gap && p.gap};
+  ${(p) => p.extraStyles && p.extraStyles};
 `;
